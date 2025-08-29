bf319aa1d63eb04b11d32777a58553ef70c5013b
REPOSITARY <21arnavsaini@gmail.com>
2025-08-29 20:32:20 +0530

    DAY9 Q2

---
+++ CODEQ18.c
@@ -0,0 +1,25 @@
+//Q18: Write a program to assign grades based on a percentage input.
+#include <stdio.h>
+
+int main() {
+    int percentage;
+
+    // Input percentage
+    printf("Provide percentage: ");
+    scanf("%d", &percentage);
+
+    // Grade assignment logic
+    if (percentage >= 90) {
+        printf("Grade A\n");
+    } else if (percentage >= 80) {
+        printf("Grade B\n");
+    } else if (percentage >= 70) {
+        printf("Grade C\n");
+    } else if (percentage >= 60) {
+        printf("Grade D\n");
+    } else {
+        printf("Grade F\n");
+    }
+
+    return 0;
+}
