66709259ef0d4dc9d6383ca0ec751f7e283939dc
21arnavsaini <21arnavsaini@gmail.com>
2025-09-07 14:26:38 +0530

    Update 1CODE.C

--- 1CODE.C
+++ 1CODE.C
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Voting and Driving eligibilty code
 #include <stdio.h>
 
@@ -17,24 +16,4 @@ int main() {
         printf("You are not eligible to drive.\n");
 
     return 0;
-=======
-// Voting and Driving eligibilty code
-#include <stdio.h>
-
-int main() {
-    int age;
-
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    if (age >= 18)
-        printf(" You are eligible to vote.\n");
-    else
-        printf(" You are not eligible to vote.\n");
-    if (age >= 18)
-        printf("You are eligible to drive.\n");
-    else
-        printf("You are not eligible to drive.\n");
-
-    return 0;
->>>>>>> 0bbb1b77627672c009d309834ff6833d574d587d
 }
