2f9b0b7d784238abe7adb5437bd52f9e0bf1ebee
REPOSITARY <21arnavsaini@gmail.com>
2025-09-07 14:35:02 +0530

    ELIGIBILTY TEST FOR VOTING AND DRIVING

--- 1CODE.C
+++ 1CODE.C
@@ -1,40 +1,21 @@
-<<<<<<< HEAD
-// Voting and Driving eligibilty code
 #include <stdio.h>
 
 int main() {
     int age;
 
     printf("Enter your age: ");
-    scanf("%d", &age);
-    if (age >= 18)
-        printf(" You are eligible to vote.\n");
-    else
-        printf(" You are not eligible to vote.\n");
-    if (age >= 18)
+    if (scanf("%d", &age) != 1 || age < 0) {
+        printf("Invalid input. Please enter a valid age.\n");
+        return 1;
+    }
+
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
         printf("You are eligible to drive.\n");
-    else
+    } else {
+        printf("You are not eligible to vote.\n");
         printf("You are not eligible to drive.\n");
+    }
 
     return 0;
-=======
-// Voting and Driving eligibilty code
-#include <stdio.h>
-
-int main() {
-    int age;
-
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    if (age >= 18)
-        printf(" You are eligible to vote.\n");
-    else
-        printf(" You are not eligible to vote.\n");
-    if (age >= 18)
-        printf("You are eligible to drive.\n");
-    else
-        printf("You are not eligible to drive.\n");
-
-    return 0;
->>>>>>> 0bbb1b77627672c009d309834ff6833d574d587d
 }
