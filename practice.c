f6d16a66ea8c1d1c94cff316feb9cbeea258ed78
REPOSITARY <21arnavsaini@gmail.com>
2025-09-07 14:49:17 +0530

    practice

--- practice.c
+++ practice.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 
 int main() {
@@ -21,27 +21,3 @@ int main() {
 
     return 0;
 }
-=======
-#include <stdio.h>
-
-int main() {
-    int i, j, rows;
-
-    printf("Enter number of rows: ");
-    scanf("%d", &rows);
-
-    for(i = 1; i <= rows; i++) {
-        // Print spaces
-        for(j = 1; j <= rows - i; j++) {
-            printf(" ");
-        }
-        // Print stars
-        for(j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
->>>>>>> 2a4f49640a57514f1390d0c358f827dbac7506ab
